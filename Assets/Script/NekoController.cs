cd62530d8023183e3496693dc24d24eefdd71865
Arddhana Zhafran <arddhanaza99@gmail.com>
2017-03-15 16:27:19 +0700

    Game 1

---
+++ Assets/Script/NekoController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NekoController : MonoBehaviour
+{
+    public bool grounded = true;
+    public float jumpPower = 1;
+    private Rigidbody2D myRigibody;
+    public float NekoJumpForce = 400f;
+    // Use this for initialization
+   
+
+    void Start()
+    {
+        myRigibody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            myRigibody.AddForce(transform.up * NekoJumpForce);
+        }
+
+        
+//   if(!grounded && myRigibody.velocity.y == 0)
+//    {
+//
+//   }
+//   if(Input.touchCount == 1 && grounded == true)
+//    {
+//       myRigibody.AddForce(transform.up * NekoJumpForce);
+//      grounded = false;
+// }
+    }
+}
+   
